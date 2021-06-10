2ef7ce978644e669ce39853cfeee2c05f7cfd105
Drakoroth <ranga.jaya@hotmail.fr>
2021-06-10 09:51:01 +0200

    callGeocoder function added in order to retrieve coordinates from a place

--- src/services.js
+++ src/services.js
@@ -1,7 +1,28 @@
 const axios = require('axios');
 
+const geocode_google = require('../config/geocode_auth');
 
 module.exports = {
+    callGeocoder: function(address) {
+        return new Promise((resolve, reject) => {
+            try {
+                geocode_google.res_geocoder(address)
+                    .then((res) => {
+                        let coord = {
+                            latitude: res[0].latitude,
+                            longitude: res[0].longitude
+                        }
+                        resolve(coord);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            } catch (err) {
+                throw (err);
+            }
+        });
+    },
+    
     callApi: function (url) {
         return new Promise((resolve, reject) => {
             try {
