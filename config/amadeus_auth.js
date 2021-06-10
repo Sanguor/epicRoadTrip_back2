7487668a1b44fae1cf2ac514b1948f649ef25d56
Drakoroth <ranga.jaya@hotmail.fr>
2021-06-10 09:49:52 +0200

    crendentials for using the Amadeus API

---
+++ config/amadeus_auth.js
@@ -0,0 +1,8 @@
+var Amadeus = require('amadeus');
+
+var amadeus = new Amadeus({
+  clientId: 'FJUfiDtrsA2kCPOtntAOb3AF6Ggasswj',
+  clientSecret: 'OTsKhqPgCnbbqSY9'
+});
+
+exports.amadeus = amadeus;
