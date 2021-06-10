971b58220328077cff89dddcd6da3ef212ea47b4
Drakoroth <ranga.jaya@hotmail.fr>
2021-06-10 09:53:56 +0200

    getNamesAndCoordinates function added to retrieve only the names and the coordinates of the places found

--- src/utils.js
+++ src/utils.js
@@ -8,5 +8,17 @@ module.exports = {
             }
         }
         return err;
+    },
+
+    getNamesAndCoordinates: function (tab){
+        let result = [];
+        let i = 0;
+        for(i = 0; i < tab.length; i++){
+            result[i] = {
+                nom: tab[i].name,
+                coordinates: tab[i].geoCode
+            }
+        }
+        return result;
     }
 }
